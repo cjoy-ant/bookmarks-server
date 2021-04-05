7979cee317845daa74c13cac235663b1408afee1
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-05 10:55:16 -0700

    install posgrator as dependency, update DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN || "dummy-api-token",
-  DB_URL:
-    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bookmarks",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgres://treonqrusyxlwk:c3bb44d0e3ec572a6d8ae4c832e81b4deb6433864a76f48d19fc50332348da6a@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d4bb1f7tlkgner",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/bookmarks-test",
 };
